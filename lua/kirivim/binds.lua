d85fe278b4b1b74fc2c470888d217eb84082cdf3
Kiri <56218513+kiriDevs@users.noreply.github.com>
2024-04-06 14:49:28 +0200

    feat: Unbind . in normal mode

--- lua/kirivim/binds.lua
+++ lua/kirivim/binds.lua
@@ -8,3 +8,7 @@ vim.keymap.set("n", "<leader>H", vim.cmd.nohlsearch)
 vim.keymap.set("n", "<leader>t", vim.cmd.term)
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 vim.keymap.set("t", "<C-^>", "<Esc>") -- Be able to send ESC for interactivity
+
+-- Unbind / Override undesired default binds
+local nop = function() end
+vim.keymap.set("n", ".", nop)
