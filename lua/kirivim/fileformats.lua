fa2a231182f2de15ed0e9f3a9b2a94b488d1e152
Kiri <56218513+kiriDevs@users.noreply.github.com>
2025-01-31 01:55:46 +0100

    feat: Map .hypr → hyprlang; rework filetype assocs

--- lua/kirivim/fileformats.lua
+++ lua/kirivim/fileformats.lua
@@ -1,13 +1,23 @@
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    pattern = { "*.typ" },
-    callback = function()
-        vim.opt.filetype = "typst"
-    end
-})
+local create_filetype = function(patterns, filetype, side_effect)
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+        pattern = patterns,
+        callback = function()
+            vim.opt.filetype = filetype
+            local se_type = type(side_effect)
+            if se_type == "function" then
+                side_effect()
+            else
+                print(
+                    "kirivim:fileformats: " .. filetype .. ": "
+                    .. "Invalid type(side_effect) = " .. se_type
+                )
+            end
+        end
+    })
+end
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    pattern = { "bun.lock" },
-    callback = function ()
-        vim.opt.filetype = "jsonc" -- Not ideal (trailing commas), close enough
-    end
-})
+create_filetype({ "*.typ" }, "typst")
+create_filetype({ "bun.lock" }, "jsonc") -- Not ideal (trailing commas); close enough?
+create_filetype({ "*.hypr" }, "hyprlang", function()
+    vim.opt.wrap = false
+end)
