b8a8d756f440b1a061c18ba0466bd2bacd3128b9
kiriDevs <56218513+kiriDevs@users.noreply.github.com>
2025-05-15 02:15:36 +0200

    style: Use lazy.nvim setup properly

--- lua/kirivim/plugins/lsp.lua
+++ lua/kirivim/plugins/lsp.lua
@@ -57,10 +57,7 @@ return {
     dependencies = {
         {
             "neovim/nvim-lspconfig",
-            {
-                "williamboman/mason.nvim",
-                config = function() require("mason").setup({}) end
-            },
+            { "williamboman/mason.nvim", config = true },
         },
     },
     config = function()
